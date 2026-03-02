603b93fce53df8eb71432f076b766b9ec1badc22
andrejflorek <andrejflorek2@gmail.com>
2026-03-02 20:51:52 +0100

    kurz JS

---
+++ script.js
@@ -0,0 +1,118 @@
+
+
+
+
+let age = 20
+
+if(age >= 18) {
+    console.log("Si dospely")
+}
+if(age <= 18) {
+    console.log("Si ddieta") 
+}
+
+
+//David Andrej Matuš
+
+
+let firstName = "Ivan"
+if(firstName === "David"){
+    console.log("Dobry den David možeš vstupit")
+} else if(firstName === "Andrej"){
+    console.log("DObry den Andrej možeš vstupit")
+} else if(firstName === "Matuš"){
+    console.log("Dobry den Matuš možeš vstupit")
+} else {
+    console.log("Nemožeš vstupit")
+}
+ 
+
+let pozicia = "administrator"
+
+if(pozicia === "programator"){
+    console.log("Aky je rozdiel medzi HTML,CSS a JavaScript")
+} else if(pozicia === "administrator"){
+    console.log("Aky poznate druh sieti")
+} else if (pozicia === "sekretarka"){
+    console.log("Ake poznate funkcie v Exceli ?")
+} else{
+    console.log("Nie ste vhodny adept") 
+}
+
+let secondName = "Andrej"
+let height = 185
+if (secondName === "Andrej" && height === 185 ){
+    console.log("Treba ho preverit")
+}
+
+let thirdName = "Andrej"
+let height1 = 222
+if ( thirdName === "Andrej" || height1 === 185 ){
+    console.log("Treba ho preverit")
+}
+
+
+/* && niečo a niečo
+true = true, true
+false = false, true
+false = true, false
+false = false, false
+
+|| niečo ALEBO niečo 
+true = true, true
+true = false, true
+true = true, false
+false = false, false
+ */
+
+let teplota = 29
+
+if (teplota <= 20 ){
+    console.log("Je chladno")
+} else if (teplota >= 21 && teplota <=26 ){
+    console.log("Je teplo")
+} else if (teplota >= 26 && teplota <=30){
+    console.log("Je horuco")
+}
+
+
+
+
+
+
+
+
+
+/*et age = 18
+
+let child = 15
+let result = child > age
+console.log ("Dieťa: " + result)
+
+let adult = 18
+let result1 = adult >= age
+console.log ("Dospely: " + result1)
+
+let pensioner = 68
+let result2 = pensioner >= age
+console.log ("Dochdoca: " + result2)
+*/
+
+
+
+
+
+
+/*
+let firstName = "Andrej"
+let age = 25
+let adult = false
+//bolean -operatory true false atd
+let result = age === 21
+console.log(result)*/
+
+
+/*
+
+=== rovna sa 
+!== nerovna sa*/
